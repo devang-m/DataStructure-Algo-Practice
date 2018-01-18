cc226607c80de7cfbc9af38d3e94f41b1d0cf975
Devang Maskara <devang.maskara@gmail.com>
2018-01-19 07:46:12 +0800

    Fenwick Tree - Range min query

--- fenwickTree.cpp
+++ fenwickTree.cpp
@@ -1,4 +1,5 @@
-//http://www.spoj.com/problems/FENTREE/
+// http://www.spoj.com/problems/FENTREE/
+// Fenwick tree implementation for range sum query
 #include <bits/stdc++.h>
 #define loop(i,n) for (int i = 0; i < n; i++)
 using namespace std;
