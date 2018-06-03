340de290ecdd86c0eb6d2c9817f541c68f500e2b
Devang Maskara <devang.maskara@gmail.com>
2018-06-03 23:31:50 +0600

    Added LIS Description

--- DynamicProgramming/LIS.cpp
+++ DynamicProgramming/LIS.cpp
@@ -7,6 +7,16 @@
 typedef long long ll;
 using namespace std;
 
+/*
+In general, we have set of active lists of varying length. We are adding an element v[i] to these lists. 
+We scan the lists (for end elements) in decreasing order of their length. We will verify the end elements of all the lists to find a list whose end element is smaller than v[i]. 
+1. If v[i] is smallest among all end candidates of active lists, we will start new active list of length 1
+2. If v[i] is largest among all end candidates of active lists, we will clone the largest active list, and extend it by v[i].
+3. If v[i] is in between, we will find a list with largest end element that is smaller than v[i]. 
+  Clone and extend this list by v[i]. We will discard all other lists of same length as that of this modified list.
+
+  Source of description : Geeks for Geeks
+  */
 int main()
 {
 	int n,k;
@@ -26,7 +36,7 @@ int main()
 			ans[0]=v[i];
 		else if(v[i]>ans[len-1])
 			ans.pb(v[i]);
-		
+		// Using binary search to clone and extend the active list in ans, by v[i]
 		else
 		{
 			int l,r;
